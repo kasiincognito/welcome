87396bc1727b633fed75c646e27523a9332d4708
kasiincognito <140201231+kasiincognito@users.noreply.github.com>
2024-02-06 18:46:32 +0300

    Add files via upload
    this is a little introduction to my dating site project

---
+++ script.js
@@ -0,0 +1,8 @@
+var div1 = document.querySelector(".name")
+var enter = document.querySelector('.enter')
+var call = document.querySelector(".call")
+
+enter.addEventListener("click",function(){
+    div1.style.opacity = 0
+    alert()
+})
